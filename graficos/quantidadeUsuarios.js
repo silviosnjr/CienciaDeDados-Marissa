9180427e99233f98f502bdef6fe5281159886ed0
silviosnjr <silviosnjr@gmail.com>
2024-10-31 11:42:40 -0300

    ajustes

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -43,7 +43,7 @@ async function quantidadeUsuarios() {
         yaxis: {
             tickfont: tickConfig,
             title: {
-                text: "Bilhoes de estudantes ativos",
+                text: "Estudantes ativos",
                 font: {
                     color: getCSS('--secundary-color')
                 }
